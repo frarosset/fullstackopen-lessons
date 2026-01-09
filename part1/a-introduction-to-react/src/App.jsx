76ef9e73fab5d9fcb2f8bd2b51884dbe6d9fa270
frarosset <37275581+frarosset@users.noreply.github.com>
2026-01-09 18:44:31 +0100

    part1: do lesson a (Introduction to React)

---
+++ part1/a-introduction-to-react/src/App.jsx
@@ -0,0 +1,46 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+
+  const friends = [
+    { name: "Peter", age: 4 },
+    { name: "Maya", age: 10 },
+  ];
+
+  const friendsNames = ["Peter", "Maya"];
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={"Maya"} age={26 + 10} />
+      <Hello name={name} age={age} />
+
+      <div>
+        {" "}
+        <p>
+          {friends[0].name} {friends[0].age}
+        </p>
+        <p>
+          {friends[1].name} {friends[1].age}
+        </p>
+      </div>
+
+      <div>
+        <p>{friendsNames}</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
