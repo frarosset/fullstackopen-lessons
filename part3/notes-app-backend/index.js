f1a19ab42eaa12ebb00f743f2193b97ebb044582
frarosset <37275581+frarosset@users.noreply.github.com>
2026-05-06 16:40:25 +0200

    part3: continue lesson c (Saving data to MongoDB)

--- part3/notes-app-backend/index.js
+++ part3/notes-app-backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+require("dotenv").config();
+const Note = require("./models/note");
+
 // const cors = require("cors");
 const app = express();
 
@@ -39,18 +42,19 @@ app.use(requestLogger);
 // });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find().then((notes) => res.json(notes));
 });
 
 app.get("/api/notes/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find((note) => note.id === id);
 
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  Note.findById(id).then((note) => res.json(note));
+
+  // if (note) {
+  //   res.json(note);
+  // } else {
+  //   res.status(404).end();
+  // }
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -60,12 +64,6 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
-
 app.post("/api/notes", (req, res) => {
   const body = req.body;
 
@@ -73,15 +71,12 @@ app.post("/api/notes", (req, res) => {
     return res.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  res.json(note);
+  note.save().then((savedNote) => res.json(savedNote));
 });
 
 const unknownEndpoint = (request, response) => {
